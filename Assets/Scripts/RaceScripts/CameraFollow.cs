eb3a3da141042be32e86e24903d292040816494f
Lily <simomoe@icloud.com>
2024-08-06 12:19:42 +1200

    king of the jungle
    king of the jungle

--- Assets/Scripts/RaceScripts/CameraFollow.cs
+++ Assets/Scripts/RaceScripts/CameraFollow.cs
@@ -3,8 +3,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
  
-    [SerializeField] private Transform target1; 
-    [SerializeField] private Transform target2; 
+    [SerializeField] private Transform target; 
     [SerializeField] private float smoothTime = 0.3f; 
 
     private Vector3 _currentVelocity = Vector3.zero;
@@ -12,30 +11,12 @@ public class CameraFollow : MonoBehaviour
 
     private void Awake()
     {
-       if (target1 == null || target2 == null)
-        {
-            Debug.LogError("Target1 and Target2 must be assigned.");
-            enabled = false; 
-            return;
-        }
-        UpdateOffset();
+       _offset = transform.position - target.position;
     }
 
     private void LateUpdate()
     {
-        if (target1 == null || target2 == null)
-        return;
-
-        Vector3 midpoint = (target1.position + target2.position) / 2;
-
-        Vector3 targetPosition = midpoint + _offset;
-
+        Vector3 targetPosition = target.position + _offset;
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _currentVelocity, smoothTime);
-        transform.LookAt(midpoint);
-    }
-     private void UpdateOffset()
-    {
-        Vector3 midpoint = (target1.position + target2.position) / 2;
-        _offset = transform.position - midpoint;
     }
 }
