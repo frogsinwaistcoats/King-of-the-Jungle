f4ecf09dc8fe91e3505919348c39c434bf3d888c
Millie <millie@katproductions.co.uk>
2024-09-11 21:14:48 +1200

    not much lol

---
+++ Assets/InstructionsManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InstructionsManager : MonoBehaviour
+{
+    public void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Maze/MazePlayerInput.cs
+++ Assets/Scripts/Maze/MazePlayerInput.cs
@@ -107,6 +107,8 @@ public class MazePlayerInput : MonoBehaviour
         {
             hasFinished = true;
             finishManager.PlayerFinish(playerID);
+
+            //if
         }
     }
 
