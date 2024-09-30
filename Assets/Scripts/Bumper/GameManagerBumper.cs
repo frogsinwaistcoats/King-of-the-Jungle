1b2b178f78a9205f1e4d7f43d464fa2c217564a5
badwolfcrown <163785002+badwolfcrown@users.noreply.github.com>
2024-09-30 19:16:40 +1300

    edited bumper
    Things to add : serializedield so i can dag and drop all the player so when they fall they get detected by the boundary

--- Assets/Scripts/Bumper/GameManagerBumper.cs
+++ Assets/Scripts/Bumper/GameManagerBumper.cs
@@ -1,12 +1,24 @@
+using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
-
 public class GameManagerBumper : MonoBehaviour
 {
-    public GameObject Player;
+   [SerializeField] GameObject Player;
     public GameObject Boundary;
+
+    public static GameManagerBumper instance;
+
+    public TextMeshProUGUI[] playerFinishText;
+    public TextMeshProUGUI gameFinishText;
+
+    private static int finishedPlayers = 0;
+    private static int totalPlayers;
+
+    GameManager gameManager;
+    SceneLoader sceneLoader;
+    TimerBumper _timeBump;
+    
 
     private void OnTriggerEnter(Collider other)
     {
@@ -17,5 +29,82 @@ public class GameManagerBumper : MonoBehaviour
         }
 
     }
+    private void Awake()
+    {
+        instance = this;
+        gameManager = GameManager.instance;
+        sceneLoader = SceneLoader.instance;
+       _timeBump = FindObjectOfType<TimerBumper>();
+
+        for (int i = 0; i < playerFinishText.Length; i++)
+        {
+            if (playerFinishText[i] != null)
+            {
+                playerFinishText[i].enabled = false;
+            }
+        }
+
+        gameFinishText.enabled = false;
+    }
+
+    private void Start()
+    {
+        totalPlayers = gameManager.players.Count;
+    }
+
+    public int PlayerFinish(int id)
+    {
+        finishedPlayers++;
+        playerFinishText[id].enabled = true;
+        playerFinishText[id].text = finishedPlayers + " !";
+        Debug.Log("Finished players: " + finishedPlayers);
+
+        if (finishedPlayers == (totalPlayers - 1) || finishedPlayers == (totalPlayers))
+        {
+            GameFinish();
+        }
+
+        return finishedPlayers;
+    }
+
+    public void GameFinish()
+    {
+        _timeBump.CancelTimer();
+        gameFinishText.enabled = true;
+        StartCoroutine(NextScene());
+    }
+
+    public int CalculateScore(int placing)
+    {
+        int score = 0;
+
+        if (placing == 1)
+        {
+            score = totalPlayers - 1;
+        }
+        else if (placing == 2)
+        {
+            score = totalPlayers - 2;
+        }
+        else if (placing == 3)
+        {
+            score = totalPlayers - 3;
+        }
+        else if (placing == 4)
+        {
+            score = totalPlayers - 4;
+        }
+
+        return score;
+    }
+
+    IEnumerator NextScene()
+    {
+        yield return new WaitForSeconds(3);
+        sceneLoader.SetPreviousScene();
+        finishedPlayers = 0;
+        SceneManager.LoadScene("Scores");
+    }
+
 
 }
