ff9c13948bfc4e2ab2cc74b11afd1d442830fa78
frogsinwaistcoats <millie@katproductions.co.uk>
2024-10-07 16:39:20 +1300

    maze stuff

--- Assets/Scripts/Maze/SpinHandler.cs
+++ Assets/Scripts/Maze/SpinHandler.cs
@@ -12,61 +12,98 @@ public class SpinHandler : MonoBehaviour
         mazePlayerInput = GetComponent<MazePlayerInput>();
     }
 
-    public void StartSpin()
+    public void StartSpin(int spinNum, Vector3 spinPos)
     {
-        StartCoroutine(SpinPlayer());
+        StartCoroutine(SpinPlayer(spinNum, spinPos));
     }
 
 
-    private IEnumerator SpinPlayer()
+    private IEnumerator SpinPlayer(int spinNum, Vector3 spinPos)
     {
         mazePlayerInput.OnDisable();
 
         Rigidbody rb = GetComponent<Rigidbody>();
-        if (rb != null )
+        if (rb != null)
         {
             rb.velocity = Vector3.zero;
             rb.angularVelocity = Vector3.zero;
         }
+
+        //Vector3 initialPosition = transform.position;
 
         float timeElapsed = 0f;
         float rotationSpeed = (1f * 360f) / spinDuration;
 
         while (timeElapsed < spinDuration)
         {
+            transform.position = spinPos;
+
             transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
             timeElapsed += Time.deltaTime;
             yield return null;
         }
 
-        SpinControls(mazePlayerInput.playerID);
+        if (rb != null)
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+
+        if (spinNum == 1)
+        {
+            SpinControls1(mazePlayerInput.playerID);
+        }
+        else if (spinNum == 2)
+        {
+            SpinControls2(mazePlayerInput.playerID);
+        }
+        else if (spinNum == 3)
+        {
+            SpinControls3(mazePlayerInput.playerID);
+        }
     }
 
-    private void SpinControls(int ID)
+    public void SpinControls1(int ID)
     {
         InputControls inputControls = mazePlayerInput.inputControls;
         if (mazePlayerInput.playerID == ID)
         {
             inputControls = mazePlayerInput.inputManager.players[mazePlayerInput.playerID].playerControls;
+            inputControls.SpinControl1.Movement.performed += mazePlayerInput.OnMove;
+            inputControls.SpinControl1.Movement.canceled += mazePlayerInput.OnMove;
+        }
+    }
 
-            //randomly generate number
-            int randomControl = Random.Range(1, 4);
+    public void SpinControls2(int ID)
+    {
+        InputControls inputControls = mazePlayerInput.inputControls;
+        if (mazePlayerInput.playerID == ID)
+        {
+            inputControls = mazePlayerInput.inputManager.players[mazePlayerInput.playerID].playerControls;
+            inputControls.SpinControl2.Movement.performed += mazePlayerInput.OnMove;
+            inputControls.SpinControl2.Movement.canceled += mazePlayerInput.OnMove;
+        }
+    }
 
-            switch (randomControl)
-            {
-                case 1:
-                    inputControls.SpinControl1.Movement.performed += mazePlayerInput.OnMove;
-                    inputControls.SpinControl1.Movement.canceled += mazePlayerInput.OnMove;
-                    break;
-                case 2:
-                    inputControls.SpinControl2.Movement.performed += mazePlayerInput.OnMove;
-                    inputControls.SpinControl2.Movement.canceled += mazePlayerInput.OnMove;
-                    break;
-                case 3:
-                    inputControls.SpinControl3.Movement.performed += mazePlayerInput.OnMove;
-                    inputControls.SpinControl3.Movement.canceled += mazePlayerInput.OnMove;
-                    break;
-            }
+    public void SpinControls3(int ID)
+    {
+        InputControls inputControls = mazePlayerInput.inputControls;
+        if (mazePlayerInput.playerID == ID)
+        {
+            inputControls = mazePlayerInput.inputManager.players[mazePlayerInput.playerID].playerControls;
+            inputControls.SpinControl3.Movement.performed += mazePlayerInput.OnMove;
+            inputControls.SpinControl3.Movement.canceled += mazePlayerInput.OnMove;
+        }
+    }
+
+    public void MasterControls(int ID)
+    {
+        InputControls inputControls = mazePlayerInput.inputControls;
+        if (mazePlayerInput.playerID == ID)
+        {
+            inputControls = mazePlayerInput.inputManager.players[mazePlayerInput.playerID].playerControls;
+            inputControls.MasterControls.Movement.performed += mazePlayerInput.OnMove;
+            inputControls.MasterControls.Movement.canceled += mazePlayerInput.OnMove;
         }
     }
 }
