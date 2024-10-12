4bfc10f4b279a69a084be87772f41a69a3819c99
badwolfcrown <163785002+badwolfcrown@users.noreply.github.com>
2024-10-12 17:34:26 +1300

    edited the scored for bumper

--- Assets/Scripts/Bumper/GameManagerBumper.cs
+++ Assets/Scripts/Bumper/GameManagerBumper.cs
@@ -4,7 +4,7 @@ using TMPro;
 using UnityEngine.SceneManagement;
 public class GameManagerBumper : MonoBehaviour
 {
-   [SerializeField] GameObject Player;
+   
     public GameObject Boundary;
 
     public static GameManagerBumper instance;
@@ -12,7 +12,6 @@ public class GameManagerBumper : MonoBehaviour
     public TextMeshProUGUI[] playerFinishText;
     public TextMeshProUGUI gameFinishText;
 
-    private static int finishedPlayers = 0;
     private static int totalPlayers;
 
     GameManager gameManager;
@@ -29,49 +28,6 @@ public class GameManagerBumper : MonoBehaviour
         }
 
     }
-    private void Awake()
-    {
-        instance = this;
-        gameManager = GameManager.instance;
-        sceneLoader = SceneLoader.instance;
-
-        if ( sceneLoader == null)
-        {
-            sceneLoader = FindAnyObjectByType<SceneLoader>();
-        }
-
-       timer = FindObjectOfType<BumperTimer>();
-
-        for (int i = 0; i < playerFinishText.Length; i++)
-        {
-            if (playerFinishText[i] != null)
-            {
-                playerFinishText[i].enabled = false;
-            }
-        }
-
-        gameFinishText.enabled = false;
-    }
-
-    private void Start()
-    {
-        totalPlayers = gameManager.players.Count;
-    }
-
-    public int PlayerFinish(int id)
-    {
-        finishedPlayers++;
-        playerFinishText[id].enabled = true;
-        playerFinishText[id].text = finishedPlayers + " !";
-        Debug.Log("Finished players: " + finishedPlayers);
-
-        if (finishedPlayers == (totalPlayers - 1) || finishedPlayers == (totalPlayers))
-        {
-            GameFinish();
-        }
-
-        return finishedPlayers;
-    }
 
     public void GameFinish()
     {
@@ -82,35 +38,12 @@ public class GameManagerBumper : MonoBehaviour
         StartCoroutine(NextScene());
     }
 
-    public int CalculateScore(int placing)
-    {
-        int score = 0;
-
-        if (placing == 1)
-        {
-            score = totalPlayers - 1;
-        }
-        else if (placing == 2)
-        {
-            score = totalPlayers - 2;
-        }
-        else if (placing == 3)
-        {
-            score = totalPlayers - 3;
-        }
-        else if (placing == 4)
-        {
-            score = totalPlayers - 4;
-        }
-
-        return score;
-    }
-
+   
     IEnumerator NextScene()
     {
-        yield return new WaitForSeconds(3);
 
-        finishedPlayers = 0;
+        GameFinish(); 
+        yield return new WaitForSeconds(3);
         SceneManager.LoadScene("Scores");
     }
 
--- Assets/Scripts/Bumper/ScoreManagerBumper.cs
+++ Assets/Scripts/Bumper/ScoreManagerBumper.cs
@@ -7,18 +7,52 @@ public class ScoreManagerBumper : MonoBehaviour
 {
     public static ScoreManagerBumper instance;
     int score = 0;
+    private int currentScore;
+    private int timer;
+    public Text scoreText;// Use this for initialization
+
     private void Awake()
     {
         instance = this;
     }
-    public void AddPoint()
+    void Start()
     {
-            score += 1;
+        currentScore = 0;
+
     }
-    public void LosePoint()
+    private void HandleScore()
     {
-        score = 0;
+        scoreText.text = "Score: " + currentScore;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            currentScore++;
+            HandleScore();
+        }
     }
 
 
+    void Update()
+    {
+
+        score = (int)Time.time; 
+
+        if (timer > 5f)
+        {
+
+            score += 2;
+
+            //We only need to update the text if the score changed.
+            scoreText.text = score.ToString();
+
+            //Reset the timer to 0.
+            timer = 0;
+        }
+    }
 }
+
+
+
