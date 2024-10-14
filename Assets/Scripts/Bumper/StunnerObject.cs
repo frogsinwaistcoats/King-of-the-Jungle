b220fb9f7dcfe699098036bc1bea745be4129f5c
badwolfcrown <163785002+badwolfcrown@users.noreply.github.com>
2024-10-15 00:11:49 +1300

    edited my scene appearance

--- Assets/Scripts/Bumper/StunnerObject.cs
+++ Assets/Scripts/Bumper/StunnerObject.cs
@@ -2,13 +2,17 @@ using UnityEngine;
 
 public class StunnerObject : MonoBehaviour
 {
-   
+    public AudioSource tickSource;
+    private void Start()
+    {
+        tickSource = GetComponent<AudioSource>();
+    }
     private void OnCollisionEnter(Collision collision)
     {
         
         if (collision.gameObject.CompareTag("Player"))
         {
-            
+            tickSource.Play();
             PlayerStun playerStun = collision.gameObject.GetComponent<PlayerStun>();
             if (playerStun != null)
             {
